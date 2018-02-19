49d4f26b04e488c78962ad0d59fef9c3009e060a
Ken Nitipat <k-e-n_ken@hotmail.com>
2018-02-19 16:16:47 +0700

    bell chart success with mock csv

--- file-server/public/javascripts/bell-chart.js
+++ file-server/public/javascripts/bell-chart.js
@@ -1,3 +1,9 @@
+function gaussian_pdf(x, mean, sigma) {
+	var gaussianConstant = 1 / Math.sqrt(2 * Math.PI),
+    x = (x - mean) / sigma;
+    return gaussianConstant * Math.exp(-.5 * x * x) / sigma;
+};
+
 // set the dimensions and margins of the graph
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
 width = 960 - margin.left - margin.right,
@@ -13,8 +19,8 @@ var yAxis = d3.axisLeft().scale(y).ticks(5);
 
 //Define the line
 var valueline = d3.line()
-.x(function(d) { return x(d.force); })
-.y(function(d) { return y(d.occur); })
+.x(function(d) {  return x(d.value); })
+.y(function(d) {  return y(d.distribution); })
 .curve(d3.curveCatmullRom.alpha(0.5));
 
 //Adds the svg canvas
@@ -26,32 +32,48 @@ var svg = d3.select("#bell-chart")
 .attr("transform",
         "translate("+margin.left+","+margin.top+")");
 
-d3.csv("data.csv", function(error, data) {
-data.forEach(function(d) {
-    d.force = +d.force;
-    d.occur = +d.occur;
-    // console.log(d)
+d3.csv("BellCurve.csv", function(error, data) {
+    data.sort(function(a,b) {
+        return d3.descending(+a.value, +b.value);
+    });
+
+    var sum = 0, count = 0, values = [];
+    data.forEach(function(d) {
+        d.value = +d.value;
+
+        values.push(d.value)
+        sum += d.value;
+        count ++;
+    });
+    var average = sum/count;
+    var std = math.std(values);
+    console.log('average: '+average);
+    console.log('std: '+std);
+
+    data.forEach(function(d) {
+        d.distribution = +gaussian_pdf(d.value, average, std);
+        // console.log(gaussian_pdf(d.value, average, std))
+    });
+    //Scale the range of the data
+    x.domain(d3.extent(data, function(d) { return d.value; }));
+    // x.domain([0, 300]);
+    // y.domain([500, 0]);
+    y.domain([d3.max(data, function(d) { return d.distribution; }), 0]);
+
+    //Add the valueline path.
+    svg.append("path")
+        .attr("class", "line")
+        .attr("d", valueline(data));
+
+    //Add the X Axis
+    svg.append("g")
+        .attr("class", "a axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+
+    //Add the Y Axis
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
 });
 
-//Scale the range of the data
-// x.domain(d3.extent(data, function(d) { return d.force; }));
-x.domain([0, 300]);
-y.domain([500, 0]);
-// y.domain([500, d3.max(data, function(d) { return d.occur; })]);
-
-//Add the valueline path.
-svg.append("path")
-    .attr("class", "line")
-    .attr("d", valueline(data));
-
-//Add the X Axis
-svg.append("g")
-    .attr("class", "a axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis)
-
-//Add the Y Axis
-svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis)
-});
